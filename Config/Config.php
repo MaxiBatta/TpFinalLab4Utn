4226e0fb793097c2cd70c3ccf2a858e40dc7921f
MB <maxibattauz@hotmail.com>
2021-10-04 22:29:51 -0300

    Se crean constantes para consumo de API

--- Config/Config.php
+++ Config/Config.php
@@ -6,6 +6,8 @@ define("FRONT_ROOT", "/tpFinalLab4/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+define('API_KEY','4f3bceed-50ba-4461-a910-518598664c08');
+define('API_URL','https://utn-students-api.herokuapp.com/api/');
 ?>
 
 
